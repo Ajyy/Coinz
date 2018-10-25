64f065cde62d38e6e2dce5e86109ce58e107854c
s1889530 <ajy980321>
2018-10-25 13:54:38 +0100

    Commit: have not finished login interface.

--- app/src/main/java/com/example/coinz/LoginInterface.kt
+++ app/src/main/java/com/example/coinz/LoginInterface.kt
@@ -1,34 +1,62 @@
 package com.example.coinz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.google.firebase.auth.FirebaseAuth
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 
 
 class LoginInterface : AppCompatActivity() {
     private var mAuth: FirebaseAuth? = null
+    private var etUserName: EditText? = null
+    private var etPassword: EditText? = null
+    private var btnLogin: Button? = null
+    private var tvInf: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_interface)
         mAuth = FirebaseAuth.getInstance()
+        etUserName = findViewById<View>(R.id.etUserName) as EditText
+        etPassword = findViewById<View>(R.id.etPassword) as EditText
+        btnLogin = findViewById<View>(R.id.btnLogin) as Button
+        tvInf = findViewById<View>(R.id.tvInf) as TextView
+
+        var user: FirebaseUser? = mAuth?.currentUser
+        if (user == null){
+            finish()
+            startActivity(Intent(this@LoginInterface, MainActivity::class.java))
+        }
+
+        btnLogin?.setOnClickListener(View.OnClickListener {  })
     }
 
     override fun onStart() {
         super.onStart()
-        // Check is user is signed in (non-null) and update UI accordingly
+        // Check if user is signed in (non-null) and update UI accordingly
         var currentUser = mAuth!!.currentUser
         updateUI(currentUser)
     }
 
     private fun updateUI(user: FirebaseUser?) {
         if (user == null) {
-            Toast.makeText("Error: sign in failed."
+            tvInf!!.text = "Error: can not login!"
+        }
+    }
+
+    private fun validate(userName: String, password: String){
+        mAuth?.signInWithEmailAndPassword(userName, password)?.addOnCompleteListener {task ->
+            if (task.isSuccessful){
+                chec
+            }
         }
     }
 }
